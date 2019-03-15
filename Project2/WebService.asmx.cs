8297fdbb41eff7790779ba651a259e44a650257c
squince <squince@asu.edu>
2019-03-14 22:52:36 -0700

    started a webservice

---
+++ Project2/WebService.asmx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Project2
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int NumberOfAccounts()
+        {
+            int x = 0;
+            string sqlConectionString = System.Configuration.ConfigurationManager.ConnectionStrings["olympusDB"].ConnectionString;
+            string sqlSelect = "SELECT * from users";
+            //MySqlConnection sqlConnection
+            return x;
+        }
+    }
+}
