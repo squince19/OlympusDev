532d80007ba9b574d4b4d651caa037e36a6eae82
squince <squince@asu.edu>
2019-04-01 15:28:09 -0700

    loademployees function js

--- Project2/JavaScript.js
+++ Project2/JavaScript.js
@@ -25,3 +25,39 @@
         }
     });
 }
+
+var employeeArray;
+
+
+//function in progress SQ
+function LoadEmployees() {
+    var webMethod = "WebService.asmx/GetNames";
+    $.ajax({
+        type: "POST",
+        url: webMethod,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        //gets a response, it calls the function mapped to the success key here
+        success: function (msg) {
+            if (msg.d) {
+
+                employeeArray = msg.d;
+                for (i = 0; i < employeeArray.length; i++) {
+                    var pNode = document.createElement('p');
+                    pValue = employeeArray[i].fname + " " + employeeArray[i].lname;
+                    pNode.innerHTML = pValue;
+                    
+                }
+            }
+            else {
+                alert("Login Failed. Wrong username or password")
+            }
+        },
+        error: function (e) {
+            alert("boo...");
+        }
+    });
+    
+}
+
+
